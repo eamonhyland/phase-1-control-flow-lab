04d5dbf45c39b5e7353ba157d47d972dbf97a9d4
Eamon <esh2464@email.vccs.edu>
2022-10-12 12:53:19 -0400

    commit tree 01145c14532fdea307c67791d10d56783331a559

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+  describe('index.js', function () {
+    describe('scuberGreetingForFeet()', function () {
+      it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
+        expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
+      });
+    });
+  });
 }
 
 function ternaryCheckCity(){
